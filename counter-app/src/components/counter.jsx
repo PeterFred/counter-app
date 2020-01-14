c60c3e0313e7370a3fefb795047dc3300d5431ae
PeterFred <32250775+PeterFred@users.noreply.github.com>
2020-01-15 11:22:38 +1300

    Updated counters with lifestyle hooks
    Updated design with bootstrap (buttons and grid)

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0, tags: ["tag1", "tag2", "tag3"] };
-
   render() {
     return (
-      <div>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag} </li>
-          ))}
-        </ul>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
